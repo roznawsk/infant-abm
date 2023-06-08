493ddb07d8801a0a1ddb0fbe4fb8413a3c66a2db
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2023-06-09 00:18:03 +0200

    Heatmap

--- cont_model/simulation.py
+++ cont_model/simulation.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import time
 import multiprocessing
+import tqdm
 
 from ast import literal_eval
 
@@ -34,99 +35,37 @@ def single_run_param_set(param_set):
     }
 
 
-def worker_function(queue, param_set):
-    result = single_run_param_set(param_set)
-    queue.put(result)
-
-
-def perform_parallel_run(param_set):
-    pool = multiprocessing.Pool()
-    manager = multiprocessing.Manager()
-    queue = manager.Queue()
-
-    repeats = param_set['repeats']
-
-    for i in range(repeats):
-        pool.apply_async(worker_function, args=(queue, param_set))
-
-    pool.close()
-
+def run_param_set(param_set):
     run_results = []
-    for _ in range(repeats):
-        result = queue.get()
-        run_results.append(result)
 
-    pool.join()
+    for _ in range(param_set['repeats']):
+        run_results.append(single_run_param_set(param_set))
 
-    return {
+    run_results = {
         'goal_dist': np.average([s['goal_dist'] for s in run_results], axis=0),
         'parent': np.average([s['parent'] for s in run_results], axis=0),
         'infant': np.average([s['infant'] for s in run_results], axis=0)
     }
 
-
-def moving_average(a, n=3):
-    a = np.concatenate([([0] * (n - 1)), a])
-
-    ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
+    return list(param_set['model_params'].values()) + [param_set['repeats'],
+                                                       param_set['max_iter']] + [run_results['goal_dist'],
+                                                                                 run_results['parent'],
+                                                                                 run_results['infant']]
 
 
-def plot_run_results(param_set, run_results):
-    x = list(range(param_set['max_iter']))
+def perform_simulation(parameter_sets):
+    pool = multiprocessing.Pool()
+    result = []
 
-    average_over_steps = 500
+    for res in tqdm.tqdm(pool.imap_unordered(run_param_set, parameter_sets, chunksize=3)):
+        result.append(res)
 
-    step_stats = run_results['goal_dist']
-    parent_stats = run_results['parent']
-    infant_stats = run_results['infant']
-
-    parent_stats = moving_average(parent_stats, average_over_steps)
-    infant_stats = moving_average(infant_stats, average_over_steps)
-
-    fig, ax1 = plt.subplots(figsize=(16, 10))
-
-    ax1.plot(x, step_stats, color='r')
-    ax1.set_ylim(bottom=0, top=150)
-    ax1.set_ylabel('goal distance')
-    ax1.set_xlabel('step')
-
-    mp = param_set['model_params']
-
-    title = f' \
-    lego={mp["lego_count"]},\
-    prec={mp["precision"]},\
-    exp={mp["exploration"]},\
-    coord={mp["coordination"]},\
-    resp={mp["responsiveness"]},\
-    rel={mp["relevance"]}, \
-    avg for {param_set["repeats"]} runs \
-    '
-    ax1.set_title(title)
-
-    # ax1.plot(x, r_steps, linestyle='dashed', marker='s', color='r')
-    # ax1.set_xlabel(current_param)
-    # ax1.set_ylim(bottom=0, top=1000)
-    # ax1.set_ylabel('steps to goal', color='r')
-
-    ax2 = ax1.twinx()
-    ax2.plot(x, parent_stats, color='b')
-    ax2.set_ylabel('satisfaction')
-
-    ax2.plot(x, infant_stats, color='orange')
-    ax2.legend(['parent', 'infant'])
-    ax2.set_ylim(bottom=0)
-
-    ax2.axvline(x=average_over_steps - 1, color='grey', label='axvline - full height')
-
-    fig.tight_layout()
-    # plt.savefig(f'../../plots/big_{current_param}.png', dpi=300)
-    plt.show()
+    results = pool.map(run_param_set, parameter_sets)
+    return results
 
 
-def get_model_param_sets(default_params):
-    grid_size = 4
+def get_model_param_sets(default_params, sim_params):
+    grid_size = 2
 
     prec = np.linspace(0, 100, grid_size)
     exp = np.linspace(0, 100, grid_size)
@@ -147,15 +86,15 @@ def get_model_param_sets(default_params):
             'relevance': rl
         }
 
-        params.append({**default_params, **param_dict})
+        params.append({'model_params': {**default_params, **param_dict}, **sim_params})
 
     return params
 
 
 if __name__ == '__main__':
     grid_size = 300
-    repeats = 100
-    max_iter = 5000
+    repeats = 10
+    max_iter = 1000
 
     default_model_params = {
         'width': grid_size,
@@ -171,37 +110,16 @@ if __name__ == '__main__':
 
     columns = list(default_model_params.keys()) + ['repeats', 'max_iter', 'goal_distance',
                                                    'parent_satisfaction', 'infant_satisfaction']
-    result = []
 
-    parameter_sets = get_model_param_sets(default_model_params)
+    sim_params = {
+        'max_iter': max_iter,
+        'repeats': repeats
+    }
+
+    parameter_sets = get_model_param_sets(default_model_params, sim_params)
     n_runs = len(parameter_sets)
 
-    for idx, model_param_set in enumerate(parameter_sets):
-
-        param_set = {
-            'model_params': model_param_set,
-            'max_iter': max_iter,
-            'repeats': repeats
-        }
-
-        start = time.time()
-        run_results = perform_parallel_run(param_set)
-        print(f'run {(idx + 1)}/{n_runs} ({(idx + 1) / n_runs * 100:.2f}%) t = {((time.time() - start) * 1000):.2f} ms')
-
-        new_entry = list(model_param_set.values(
-        )) + [repeats, max_iter] + [run_results['goal_dist'], run_results['parent'], run_results['infant']]
-
-        result.append(new_entry)
-
-    # print(len(result[-1]))
+    result = perform_simulation(parameter_sets)
     out_df = pd.DataFrame(result, columns=columns)
 
-    print(len(out_df.iloc[0, :][-1]))
-
-    # store = pd.HDFStore('results/run.h5')
-    # store['df'] = out_df
-    # store = pd.HDFStore('results/run.h5')
-
-    out_df.to_hdf('results/run_grid_5.hdf', 'hdfkey')
-
-    # out_df.to_csv('results/run.csv')
+    out_df.to_hdf('results/run_new_parallel.hdf', 'hdfkey')

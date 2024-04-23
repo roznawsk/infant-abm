b6d7b6851bb224652525466302deac4b1ac00afb
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2024-04-23 10:01:57 +0200

    test run

--- mesa/run_simulation.py
+++ mesa/run_simulation.py
@@ -25,11 +25,11 @@ def get_model_param_sets(num):
 
 
 if __name__ == "__main__":
-    repeats = 3
+    repeats = 7
     iterations = 2000
     output_path = "../results/test_run_temp.hdf"
 
-    parameter_sets = get_model_param_sets((4, 4, 4))
+    parameter_sets = get_model_param_sets((8, 8, 8))
     # parameter_sets = [{"infant_params": InfantParams(perception=0.66, persistence=1, coordination=1)}]
 
     simulation = Simulation(

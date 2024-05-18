c9ed62b0e12358f019e9a76787c096b842cc1662
Przemysław Rożnawski <48837433+roznawsk@users.noreply.github.com>
2024-05-18 14:17:59 +0200

    Add vision sequence (#12)
    * Improve UI (#2)
    
    * Update readme.md
    
    * Add parent (#3)
    
    * Improve UI
    
    * Add exploration to child
    
    * Created parent
    
    * Add parent parameters
    
    * basic stats
    
    * Add parent (#4)
    
    * Improve UI
    
    * Add exploration to child
    
    * Created parent
    
    * Add parent parameters
    
    * basic stats
    
    * Add crabmaga flocker example
    
    * Graphs
    
    * Add professional stats
    
    * Fix server params
    
    * Update readme.md
    
    * Apply model improvements (#5)
    
    * Improve UI
    
    * Add exploration to child
    
    * Created parent
    
    * Add parent parameters
    
    * basic stats
    
    * Add crabmaga flocker example
    
    * Graphs
    
    * Add professional stats
    
    * Fix server params
    
    * Applied updates
    
    * Export to csv
    
    * Run grid 5
    
    * Heatmap
    
    * More precise heatmap
    
    * Heatmap detailed
    
    * New data chunk
    
    * Improve heatmap
    
    * Update heatmap
    
    * Rename (#6)
    
    * Improve UI
    
    * Add exploration to child
    
    * Created parent
    
    * Add parent parameters
    
    * basic stats
    
    * Add crabmaga flocker example
    
    * Graphs
    
    * Add professional stats
    
    * Fix server params
    
    * Applied updates
    
    * Export to csv
    
    * Run grid 5
    
    * Heatmap
    
    * More precise heatmap
    
    * Heatmap detailed
    
    * New data chunk
    
    * Improve heatmap
    
    * Update heatmap
    
    * Remove unused deps
    
    * Rename
    
    * Add infant genome.
    
    * Refactor simulation.
    
    * Move simulation to separate file
    
    * BUG
    
    * Update readme
    
    * Add infant params
    
    * PSO
    
    * Add poetry and BLACK
    
    * Py gad (#7)
    
    * Remove crabmaga
    
    * Pygad WIP
    
    * WIP
    
    * WIP
    
    * Linter (#8)
    
    * Create pylint.yml
    
    * Add ruff
    
    * fix lock
    
    * Simple search (#9)
    
    * Hardcode speed and grid size
    
    * :WIP exp/exp
    
    * Limit throw range
    
    * WIP refactor
    
    * Fixed simulation
    
    * Fix simulation and results
    
    * Revert run_simulation.py
    
    * test run
    
    * Further analisis
    
    * Add exploration-exploitation ratio (#10)
    
    * Remove grid model
    
    * Add explore_exploit_ratio
    
    * Format
    
    * Add infant vision (#11)
    
    * Finished infant vision
    
    * Add parent vision
    
    * Add visibility sequence
    
    * Add infant inherited classes|
    
    * Remove explore-exploit ratio
    
    * Add comparative test
    
    * Improve SeqVisionInfant
    
    * Add results to ignore
    
    * Remove duplicate workflow

--- mesa/run_simulation.py
+++ mesa/run_simulation.py
@@ -3,34 +3,33 @@ import itertools
 import warnings
 
 from simulation import Simulation
-from infant_abm.agents.infant import Params as InfantParams
+from infant_abm.agents.infant_base import Params as InfantParams
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
-def get_model_param_sets(num):
-    p1, p2, p3 = num
-    perception = np.linspace(0, 1, p1)
-    persistence = np.linspace(0, 1, p2)
-    coordination = np.linspace(0, 1, p3)
+def get_model_param_sets(linspace, base_params=dict()):
+    lo, hi, num = linspace
+    perception = np.linspace(lo, hi, num)
+    persistence = np.linspace(lo, hi, num)
+    coordination = np.linspace(lo, hi, num)
 
     params = []
 
     for param_set in itertools.product(*[perception, persistence, coordination]):
         i_params = InfantParams.from_array(param_set)
 
-        params.append({"infant_params": i_params})
+        params.append({**base_params, "infant_params": i_params})
 
     return params
 
 
-if __name__ == "__main__":
-    repeats = 7
+def run_basic_simulation():
+    repeats = 11
     iterations = 2000
-    output_path = "../results/test_run_temp.hdf"
+    output_path = "../results/run_no_eye_contact.hdf"
 
-    parameter_sets = get_model_param_sets((8, 8, 8))
-    # parameter_sets = [{"infant_params": InfantParams(perception=0.66, persistence=1, coordination=1)}]
+    parameter_sets = get_model_param_sets((0, 1, 8))
 
     simulation = Simulation(
         model_param_sets=parameter_sets,
@@ -40,5 +39,34 @@ if __name__ == "__main__":
         display=True,
     )
 
-    results = simulation.run()
+    simulation.run()
     simulation.save()
+
+
+def run_comparative_simulation():
+    repeats = 7
+    iterations = 5000
+
+    for parent_class in ["MoverParent", "VisionOnlyParent"]:
+        for infant_class in ["NoVisionInfant", "SeqVisionInfant"]:
+            base_params = {"parent_class": parent_class, "infant_class": infant_class}
+            parameter_sets = get_model_param_sets(
+                (0.4, 0.6, 3), base_params=base_params
+            )
+            output_path = f"../results/comparative2/{parent_class}_{infant_class}.hdf"
+
+            simulation = Simulation(
+                model_param_sets=parameter_sets,
+                iterations=iterations,
+                repeats=repeats,
+                output_path=output_path,
+                display=True,
+            )
+
+            simulation.run()
+            simulation.save()
+
+
+if __name__ == "__main__":
+    # run_basic_simulation()
+    run_comparative_simulation()

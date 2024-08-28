f124117c4f7a748a08cf6d37652530523822ecb7
Przemysław Rożnawski <48837433+roznawsk@users.noreply.github.com>
2024-08-28 15:51:09 +0200

    Improve simulation (#27)
    * Analyze distribution
    
    * optimize db
    
    * fix lock

--- infant_abm/db_utils.py
+++ infant_abm/db_utils.py
@@ -1,11 +1,13 @@
 import json
-
-from os import path
-
+import numpy as np
 import pandas as pd
 
 
-def load_run(run_path):
+from os import path
+from pathlib import Path
+
+
+def load_run_old(run_path):
     description_df = pd.read_csv(path.join(run_path, "description.csv"), index_col=0)
 
     def load_partial(index):
@@ -17,11 +19,61 @@ def load_run(run_path):
     return description_df, load_partial
 
 
-def save_partial(run_path, index, result):
+def load_run(run_path):
+    description_df = pd.read_csv(path.join(run_path, "description.csv"), index_col=0)
+
+    def load_partial(index):
+        partial_path = get_partial_path(run_path, index)
+
+        with open(partial_path, "r") as file:
+            result = json.load(file)
+
+        for repeat, rep_result in result.items():
+            for k, v in rep_result.items():
+                if path.exists(str(v)):
+                    result[repeat][k] = np.fromfile(v)
+
+        return result
+
+    return description_df, load_partial
+
+
+def partial_exists(run_path, index):
     partial_path = get_partial_path(run_path, index)
+    return path.exists(partial_path)
+
+
+def save_partial(run_path: str, index: int, result: dict):
+    partial_path = get_partial_path(run_path, index)
+    partial_dir = get_partial_dir(run_path, index)
+
+    arrays_stored = any([isinstance(v, np.ndarray) for v in result[0].values()])
+    if arrays_stored:
+        Path(partial_dir).mkdir(parents=False, exist_ok=False)
+
+    for repetition, rep_result in result.items():
+        repetition_dir = path.join(partial_dir, str(repetition))
+        if arrays_stored:
+            Path(repetition_dir).mkdir(parents=False, exist_ok=False)
+
+        for k, v in rep_result.items():
+            if isinstance(v, np.ndarray):
+                arr_path = path.join(repetition_dir, str(k))
+                # v.tofile(arr_path)
+                v = np.array(v)
+                v.tofile(arr_path)
+
+                result[repetition][k] = arr_path
+
+    # for repetition in result.keys():
+    #     del result[repetition]["boosts"]
 
     with open(partial_path, "w") as file:
         json.dump(result, file)
+
+
+def get_partial_dir(run_path, index):
+    return path.join(run_path, str(index))
 
 
 def get_partial_path(run_path, index):
--- run_simulation.py
+++ run_simulation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import itertools
 import warnings
+import os
 
 from pathlib import Path
 
@@ -43,7 +44,10 @@ class Model_0_2_0:
     output_dir = "v0.2.0"
 
 
-def get_model_param_sets(linspace, base_params=dict()):
+ITERATIONS = 20000
+
+
+def get_model_param_sets(model, linspace, base_params=dict()):
     lo, hi, num = linspace
     perception = np.linspace(lo, hi, num)
     persistence = np.linspace(lo, hi, num)
@@ -54,29 +58,26 @@ def get_model_param_sets(linspace, base_params=dict()):
     for param_set in itertools.product(*[perception, persistence, coordination]):
         i_params = InfantParams.from_array(param_set)
 
-        params.append({**base_params, "infant_params": i_params})
+        params.append(
+            {
+                **base_params,
+                "infant_params": i_params,
+                "infant_class": model.infant_class,
+                "parent_class": model.parent_class,
+            }
+        )
 
     return params
 
 
-def get_linspace_str(linspace):
-    return (
-        str(linspace)
-        .replace("(", "_")
-        .replace(")", "")
-        .replace(" ", "")
-        .replace(".", "")
-        .replace(",", "_")
-    )
-
-
-def run_basic_simulation(output_dir, parameter_sets, repeats=13, iterations=20000):
+def run_basic_simulation(output_dir, parameter_sets, repeats=100):
     simulation = Simulation(
         model_param_sets=parameter_sets,
-        iterations=iterations,
+        iterations=ITERATIONS,
         repeats=repeats,
         output_dir=output_dir,
         display=True,
+        processes=os.cpu_count() - 1,
     )
 
     simulation.run()
@@ -84,50 +85,27 @@ def run_basic_simulation(output_dir, parameter_sets, repeats=13, iterations=2000
     return simulation
 
 
-def run_comparative_simulation():
-    repeats = 11
-    iterations = 15000
-
-    for infant_class in ["SeqVisionInfant"]:
-        base_params = {"infant_class": infant_class}
-        parameter_sets = get_model_param_sets((0.1, 0.9, 5), base_params=base_params)
-        output_path = f"../results/final_final_3/{infant_class}.hdf"
-
-        simulation = Simulation(
-            model_param_sets=parameter_sets,
-            iterations=iterations,
-            repeats=repeats,
-            output_path=output_path,
-            display=True,
-        )
-
-        simulation.run()
-
-
-def run_comparative_boost_simulation():
-    linspace = (0.1, 0.9, 5)
-
-    output_dir = "./results/model1/boost_improvement"
-    Path(output_dir).mkdir(parents=False, exist_ok=False)
-
-    lo, hi, num = linspace
+def run_comparative_boost_simulation(
+    model, repeats, output_dir, linspace, boost_linspace
+):
+    output_dir = f"./results/{model.output_dir}/{output_dir}"
+    Path(output_dir).mkdir(parents=True, exist_ok=False)
 
     perception, persistence, coordination = [
-        np.round(np.linspace(lo, hi, num), 3) for _ in range(3)
+        np.round(np.linspace(*linspace), 3) for _ in range(3)
     ]
 
-    boost = np.linspace(0, 1, 5)
+    boost = np.linspace(*boost_linspace)
     params = []
 
     for param_set in itertools.product(*[perception, persistence, coordination, boost]):
-        # for bst in boost:
         prc, prs, crd, bst = param_set
-
-        # prc, prs, crd = param_set
 
         i_params = InfantParams.from_array([prc, prs, crd])
         base_params = {
-            "config": Config(persistence_boost_value=bst, coordination_boost_value=bst)
+            "config": Config(persistence_boost_value=bst, coordination_boost_value=bst),
+            "infant_class": model.infant_class,
+            "parent_class": model.parent_class,
         }
 
         params.append({**base_params, "infant_params": i_params})
@@ -135,27 +113,39 @@ def run_comparative_boost_simulation():
     run_basic_simulation(
         output_dir=output_dir,
         parameter_sets=params,
-        iterations=20000,
-        repeats=13,
+        repeats=repeats,
     )
+
+
+def run_from_description(model, output_dir, repeats):
+    output_dir = f"./results/{model.output_dir}/{output_dir}"
+
+    simulation = Simulation.from_description(
+        output_dir=output_dir,
+        iterations=ITERATIONS,
+        repeats=repeats,
+        display=True,
+        processes=os.cpu_count() - 1,
+    )
+
+    simulation.run()
+
+    return simulation
 
 
 if __name__ == "__main__":
-    model = Model_0_2_0()
-    output_dir = f"./results/{model.output_dir}/test"
+    model = Model_0_1_1()
 
-    params = [
-        {
-            "infant_params": InfantParams.from_array([0.5, 0.5, 0.5]),
-            "infant_class": model.infant_class,
-            "parent_class": model.parent_class,
-            "config": Config(),
-        }
-    ]
+    grid = 3
+    boost = 1
+    repeats = 1
+    output_dir = f"success_{grid}_grid_{boost}_boost_test_no_array"
 
-    run_basic_simulation(
-        output_dir=output_dir,
-        parameter_sets=params,
-        iterations=100,
-        repeats=1,
+    linspace = (0.05, 0.95, grid)
+    boost_linspace = (0, 1, boost)
+
+    run_comparative_boost_simulation(
+        model, repeats, output_dir, linspace, boost_linspace
     )
+
+    # run_from_description(model, output_dir, repeats)
